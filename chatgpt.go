aed4660f2c5f7d3e03aa4ca823e435cb42ecd971
qiangqiang.sun <s1656922694@163.com>
2023-06-24 20:43:12 +0800

    <feat>{  1.GPT4.5s超时返回错误提示语"你的问题太复杂了,你可以再问我一遍吗." }

--- chatgpt.go
+++ chatgpt.go
@@ -41,7 +41,8 @@ func GetChatData(appKey, content string) string {
 	var value string
 	err := cache.Get(nil, key, groupcache.StringSink(&value))
 	if err != nil {
-		return err.Error()
+		log.Fatalf("cache.Get%v\n", err.Error())
+		return "你的问题太复杂了,你可以再问我一遍吗."
 	}
 	return value
 }
