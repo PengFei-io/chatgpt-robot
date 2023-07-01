ee2cca37cd5727f85255884d99c588fe918480c4
qiangqiang.sun <s1656922694@163.com>
2023-07-01 23:55:32 +0800

    <config>{  1.docker构建 }@v2

--- restart.sh
+++ restart.sh
@@ -1,6 +1,2 @@
-pid=$(ps -ef|grep -i chat|awk '{print $2}')
-sudo kill -9 $pid
-sudo rm -f chatgpt-robot
-go build
-sudo nohup ./chatgpt-robot > myout.log 2>&1 &
-tail -f  myout.log
+sudo docker build -t robot:1.0 -f Dockerfile .
+sudo docker-compose up -d
