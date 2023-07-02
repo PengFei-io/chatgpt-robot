c8170720d35be89b6c315d15d18662d5c944fe93
qiangqiang.sun <s1656922694@163.com>
2023-07-02 22:39:25 +0800

    <feat>{  1.增加@笑话指令 }@v2

--- utils/intention/handler.go
+++ utils/intention/handler.go
@@ -7,10 +7,16 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 )
 
 // IntentHandle 意图识别,判断天气.
 func IntentHandle(fromUserName string, content string) string {
+	t0 := time.Now()
+	defer func() {
+		t1 := time.Now()
+		fmt.Printf("消耗时间:%dms\n", t1.Sub(t0).Milliseconds())
+	}()
 	if strings.Contains(content, "@天气") {
 		fmt.Println("hit_intent_天气")
 		return weather.GetWeather(content)
